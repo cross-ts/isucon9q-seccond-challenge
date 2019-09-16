c5392506a9b5367593cc63cc523afa37880e079e
cross-ts <ts.kouhei@gmail.com>
2019-09-16 03:41:41 +0000

    Fix transactions user N+1

--- isucari/webapp/ruby/lib/isucari/web.rb
+++ isucari/webapp/ruby/lib/isucari/web.rb
@@ -281,7 +281,7 @@ module Isucari
       items = if item_id > 0 && created_at > 0
         # paging
         begin
-          db.xquery("SELECT * FROM `items` WHERE (`seller_id` = ? OR `buyer_id` = ?) AND `status` IN (?, ?, ?, ?, ?) AND (`created_at` < ?  OR (`created_at` <= ? AND `id` < ?)) ORDER BY `created_at` DESC, `id` DESC LIMIT #{TRANSACTIONS_PER_PAGE + 1}", user['id'], user['id'], ITEM_STATUS_ON_SALE, ITEM_STATUS_TRADING, ITEM_STATUS_SOLD_OUT, ITEM_STATUS_CANCEL, ITEM_STATUS_STOP, Time.at(created_at), Time.at(created_at), item_id)
+          db.xquery("SELECT items.*, users.account_name, users.num_sell_items FROM `items` INNER JOIN users ON items.seller_id = users.id WHERE (`items.seller_id` = ? OR `items.buyer_id` = ?) AND (`items.created_at` < ?  OR (`items.created_at` <= ? AND `items.id` < ?)) ORDER BY `items.created_at` DESC, `items.id` DESC LIMIT #{TRANSACTIONS_PER_PAGE + 1}", user['id'], user['id'], Time.at(created_at), Time.at(created_at), item_id)
         rescue
           db.query('ROLLBACK')
           halt_with_error 500, 'db error'
@@ -289,7 +289,7 @@ module Isucari
       else
         # 1st page
         begin
-          db.xquery("SELECT * FROM `items` WHERE (`seller_id` = ? OR `buyer_id` = ?) AND `status` IN (?, ?, ?, ?, ?) ORDER BY `created_at` DESC, `id` DESC LIMIT #{TRANSACTIONS_PER_PAGE + 1}", user['id'], user['id'], ITEM_STATUS_ON_SALE, ITEM_STATUS_TRADING, ITEM_STATUS_SOLD_OUT, ITEM_STATUS_CANCEL, ITEM_STATUS_STOP)
+          db.xquery("SELECT items.*, users.account_name, users.num_sell_items FROM `items` INNER JOIN users ON items.seller_id = users.id WHERE (`seller_id` = ? OR `buyer_id` = ?) ORDER BY `created_at` DESC, `id` DESC LIMIT #{TRANSACTIONS_PER_PAGE + 1}", user['id'], user['id'] )
         rescue
           db.query('ROLLBACK')
           halt_with_error 500, 'db error'
@@ -297,11 +297,15 @@ module Isucari
       end
 
       item_details = items.map do |item|
-        seller = get_user_simple_by_id(item['seller_id'])
-        if seller.nil?
+        if item['account_name'].nil?
           db.query('ROLLBACK')
           halt_with_error 404, 'seller not found'
         end
+        seller = {
+          'id' => item['seller_id'],
+          'account_name' => item['account_name'],
+          'num_sell_items' => item['num_sell_items']
+        }
 
         category = get_category_by_id(item['category_id'])
         if category.nil?
